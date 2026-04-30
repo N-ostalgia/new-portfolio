2c0cf2360f3bcbec266c569b4b373d7cde4343eb
N-ostalgia <bouibauanaya730@gmail.com>
2026-04-30 11:25:10 +0100

    adding

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,8 +1,8 @@
 export const SKILLS = {
-  "Backend":           ["Python","FastAPI","Flask","Java","C/C++","PHP","Node.js","Electron.js"],
-  "Frontend":          ["React","HTML/CSS","JavaScript","JavaFX","Qt","Chart.js","CSS Modules"],
+  "Backend":           ["Python","FastAPI","Flask","Java","Spring framework / spring boot","C/C++","PHP","Node.js","Electron.js"],
+  "Frontend":          ["React","HTML/CSS","tailwind css","Bootstrap","JavaScript","JavaFX","Qt","Chart.js","CSS Modules"],
   "AI & Data Science": ["pandas","NumPy","Scikit-learn","XGBoost","TensorFlow","Keras","OpenCV","ResNet50"],
-  "Security & Net":    ["Wireshark","tshark","CICFlowMeter","nmap","WebSockets"],
-  "Databases":         ["MySQL","SQLite","MongoDB"],
+  "Security & Net":    ["Wireshark","tshark","CICFlowMeter","IDS" , "Snort", "nmap", "metasploit", "hydra"],
+  "Databases":         ["MySQL","SQLite","MongoDB","PostgreSQL"],
   "Tools":             ["Git","Docker","Linux","Figma","Jira","REST API","Agile/Scrum"],
 };
